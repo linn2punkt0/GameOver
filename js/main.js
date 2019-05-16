0121845c154052f9b1f26907696250fef33554a1
root <root@DESKTOP-2TKMD5S.localdomain>
2019-05-16 09:59:55 +0200

    Updated basic setup

--- js/main.js
+++ js/main.js
@@ -1,27 +1,19 @@
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
+    width: 1000,
     height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-        // [Example1]
-    }
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: {y: 200},
+        debug: false
+      }
+    },
+    scene: [ Tomato, World]
+    // create: create,
+    // preload: preload,
+    // update: update
 };
 
 var game = new Phaser.Game(config);
-
-function preload () {
-    this.load.image('tomato', 'assets/images/tomato.png');
-  }
-
-function create () {
-  const tomato = this.add.sprite(400, 300, 'tomato');
-  tomato.setScale(.08);
-}
-
-function update () {
-
-}
